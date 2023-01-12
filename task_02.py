cc5c6f7a37fa70e961c615ea592f09cb6a7772f7
EvgenyMelnik <122205912+jozeppe233@users.noreply.github.com>
2023-01-12 16:14:31 +0400

    Добавил файл task_02 с решением

---
+++ task_02.py
@@ -0,0 +1,8 @@
+#Задача №2
+#https://www.codewars.com/kata/578c1e2edaa01a9a02000b7f
+
+def alias_gen(f_name, l_name):
+    if f_name[0].isalpha() and l_name[0].isalpha():
+        return f'{FIRST_NAME[f_name[0].upper()]} {SURNAME[l_name[0].upper()]}'
+    else:
+        return 'Your name must start with a letter from A - Z.'
